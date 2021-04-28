267f119c8de9f0b599af385fcd8c9390f5fb0c58
Sam <sbrandt1240@gmail.com>
2021-04-28 16:11:36 +0300

    seperated components in FE

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,78 +1,29 @@
+import { link } from 'node:fs';
 import * as React from 'react'
-import styled from 'styled-components'
-
-type LayoutProps = {
-    gap: number,
-}
-
-const Row = styled.div<LayoutProps>`
-display:flex;
-flex-direction:row;
-gap: ${(props) => props.gap}px
-`;
-
-const Column = styled.div<LayoutProps>`
-display:flex;
-flex-direction:column;
-gap: ${(props) => props.gap}px
-`;
+import { Board } from './BoardProps';
+import { GameState, useGameState } from './GameState';
+import { Row, Column } from './LayoutProps';
+import { Log } from './LogProps';
 
 function Game() {
+    const {
+        gameState,
+        current,
+        xIsNext,
+        jumpTo,
+        winner,
+        handleClick,
+    } = useGameState();
     return (
         <Row gap={20}>
             <Column gap={20}>
-                <div>Status</div>
-                <Board />
+                <div>{
+                    winner ? `Winner ${winner}` : `Next Player ${xIsNext ? 'X' : 'O'}`
+                }</div>
+                <Board board={current} onClick={handleClick} />
             </Column>
-            <Log />
+            <Log history={gameState.history} jumpTo={jumpTo} />
         </Row>
     );
 }
-
-function Board() {
-    return (
-        <Column gap={0}>
-            <Row gap={0}>
-                <Square />
-                <Square />
-                <Square />
-            </Row>
-            <Row gap={0}>
-                <Square />
-                <Square />
-                <Square />
-            </Row>
-            <Row gap={0}>
-                <Square />
-                <Square />
-                <Square />
-            </Row>
-        </Column>
-    );
-}
-
-const StyledSquare = styled.button`
-    width:34px;
-    height: 34px;
-    background: #fff;
-    border: 1px solid #999;
-    padding:0;
-    font-size:24px;
-    font-weight:bold;
-`;
-function Square() {
-    return (
-        <StyledSquare>
-            X
-        </StyledSquare>
-    )
-}
-function Log() {
-    return (
-        <ol>
-            <li><button>Go To Move</button></li>
-        </ol>
-    );
-}
-
 export default Game;
